03897d17e77068320327e7adc1272b05b1608e9d
b.gyorfi <b.gyorfi@twinner.com>
2022-03-29 10:29:19 +0200

    Models added

--- HX0KAT_HFT_2021222.Models/Phone.cs
+++ HX0KAT_HFT_2021222.Models/Phone.cs
@@ -2,7 +2,7 @@
 
 namespace HX0KAT_HFT_2021222.Models
 {
-    public class Class1
+    public class Phone
     {
     }
 }
---
+++ HX0KAT_HFT_2021222.Models/Repairer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HX0KAT_HFT_2021222.Models
+{
+    public class Repairer
+    {
+
+    }
+}
