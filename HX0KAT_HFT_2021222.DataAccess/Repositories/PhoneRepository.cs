c65334b68015d84ac08681f71e04bd839856018f
b.gyorfi <b.gyorfi@twinner.com>
2022-03-30 17:01:16 +0200

    PhoneRepository added

---
+++ HX0KAT_HFT_2021222.DataAccess/Repositories/PhoneRepository.cs
@@ -0,0 +1,18 @@
+﻿using HX0KAT_HFT_2021222.DataAccess.Data;
+using HX0KAT_HFT_2021222.DataAccess.Interfaces;
+using HX0KAT_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HX0KAT_HFT_2021222.DataAccess.Repositories
+{
+    public class PhoneRepository : Repository<Phone>, IPhoneRepository
+    {
+        public PhoneRepository(PhoneShopDbContext ctx) : base(ctx)
+        {
+        }
+    }
+}
