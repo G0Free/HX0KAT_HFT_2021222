ee889e6c5d04f80cb22a6f298bb4ddc690b7e7a9
b.gyorfi <b.gyorfi@twinner.com>
2022-03-30 17:54:29 +0200

    Repairer logic added

---
+++ HX0KAT_HFT_2021222.Logic/Services/RepairerLogic.cs
@@ -0,0 +1,50 @@
+﻿using HX0KAT_HFT_2021222.DataAccess.Interfaces;
+using HX0KAT_HFT_2021222.Logic.Interfaces;
+using HX0KAT_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HX0KAT_HFT_2021222.Logic.Services
+{
+    public class RepairerLogic : IRepairerLogic
+    {
+        IRepairerRepository repairerRepo;
+
+        public RepairerLogic(IRepairerRepository repairerRepo)
+        {
+            this.repairerRepo = repairerRepo;
+        }
+
+        #region CRUD
+
+        public void Create(Repairer repairer)
+        {
+            repairerRepo.Create(repairer);
+        }
+
+        public void Delete(int id)
+        {
+            repairerRepo.Delete(id);
+        }
+
+        public Repairer Read(int id)
+        {
+            return repairerRepo.Read(id) ?? throw new ArgumentException("Repairer with the specified id does not exists.");
+        }
+
+        public IEnumerable<Repairer> ReadAll()
+        {
+            return repairerRepo.ReadAll();
+        }
+
+        public void Update(Repairer repairer)
+        {
+            repairerRepo.Update(repairer);
+        }
+
+        #endregion
+    }
+}
