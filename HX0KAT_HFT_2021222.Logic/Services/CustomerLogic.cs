a063bc775a9570e74ecd9ffca6231e8a62a76d5a
b.gyorfi <b.gyorfi@twinner.com>
2022-03-30 17:39:27 +0200

    CRUD methods added

---
+++ HX0KAT_HFT_2021222.Logic/Services/CustomerLogic.cs
@@ -0,0 +1,54 @@
+﻿using HX0KAT_HFT_2021222.DataAccess.Interfaces;
+using HX0KAT_HFT_2021222.Logic.Interfaces;
+using HX0KAT_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HX0KAT_HFT_2021222.Logic.Services
+{
+    public class CustomerLogic : ICustomerLogic
+    {
+        ICustomerRepository customerRepo;
+
+        public CustomerLogic(ICustomerRepository customerRepository)
+        {
+            this.customerRepo = customerRepository;
+        }
+
+        #region CRUD
+        public void Create(Customer customer)
+        {
+            customerRepo.Create(customer);
+        }
+
+        public void Delete(int id)
+        {
+            customerRepo.Delete(id);
+        }
+
+        public Customer Read(int id)
+        {
+            return customerRepo.Read(id) ?? throw new ArgumentException("Customer with the specifeid ID does not exists");
+        }
+
+        public IEnumerable<Customer> ReadAll()
+        {
+            return customerRepo.ReadAll();
+        }
+
+        public void Update(Customer customer)
+        {
+            customerRepo.Update(customer);
+        }
+        #endregion
+
+        #region NON-CRUD
+
+
+
+        #endregion
+    }
+}
