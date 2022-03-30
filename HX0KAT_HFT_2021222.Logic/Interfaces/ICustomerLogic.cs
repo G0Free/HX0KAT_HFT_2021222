1963eda482baf662c6badc32aef3b11535c48515
b.gyorfi <b.gyorfi@twinner.com>
2022-03-30 17:26:56 +0200

    CustomerLogic interfaces added

---
+++ HX0KAT_HFT_2021222.Logic/Interfaces/ICustomerLogic.cs
@@ -0,0 +1,18 @@
+﻿using HX0KAT_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HX0KAT_HFT_2021222.Logic.Interfaces
+{
+    public interface ICustomerLogic
+    {
+        void Create(Customer customer);
+        Customer Read(int id);
+        IEnumerable<Customer> ReadAll();
+        void Update(Customer customer);
+        void Delete(Customer customer);
+    }
+}
