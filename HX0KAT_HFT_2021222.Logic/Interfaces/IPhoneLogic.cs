2c887cfe94359710d1b55c94559c002daabf51ba
b.gyorfi <b.gyorfi@twinner.com>
2022-03-30 17:32:20 +0200

    Logic interfaces added

---
+++ HX0KAT_HFT_2021222.Logic/Interfaces/IPhoneLogic.cs
@@ -0,0 +1,19 @@
+﻿using HX0KAT_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HX0KAT_HFT_2021222.Logic.Interfaces
+{
+    public interface IPhoneLogic
+    {
+        void Create(Phone phone);
+        Phone Read(int id);
+        IEnumerable<Phone> ReadAll();
+        void Update(Phone phone);
+        void Delete(Phone phone);
+
+    }
+}
---
+++ HX0KAT_HFT_2021222.Logic/Interfaces/IRepairerLogic.cs
@@ -0,0 +1,18 @@
+﻿using HX0KAT_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HX0KAT_HFT_2021222.Logic.Interfaces
+{
+    public interface IRepairerLogic
+    {
+        void Create(Repairer repairer);
+        Repairer Read(int id);
+        IEnumerable<Repairer> ReadAll();
+        void Update(Repairer repairer);
+        void Delete(Repairer repairer);
+    }
+}
