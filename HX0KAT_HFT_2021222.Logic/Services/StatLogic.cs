46366bff3c14932868db1f2351d2b53ef42f72e5
b.gyorfi <b.gyorfi@twinner.com>
2022-04-27 18:21:16 +0200

    bug fixed

--- HX0KAT_HFT_2021222.Logic/Services/StatLogic.cs
+++ HX0KAT_HFT_2021222.Logic/Services/StatLogic.cs
@@ -1,4 +1,5 @@
-﻿using HX0KAT_HFT_2021222.Logic.Interfaces;
+﻿using HX0KAT_HFT_2021222.DataAccess.Interfaces;
+using HX0KAT_HFT_2021222.Logic.Interfaces;
 using HX0KAT_HFT_2021222.Models;
 using System;
 using System.Collections.Generic;
@@ -10,32 +11,64 @@ namespace HX0KAT_HFT_2021222.Logic.Services
 {
     public class StatLogic : IStatLogic
     {
-        IPhoneLogic phoneLogic;
-        ICustomerLogic customerLogic;
-        IRepairerLogic repairerLogic;
+        //IPhoneLogic phoneLogic;
+        //ICustomerLogic customerLogic;
+        //IRepairerLogic repairerLogic;
 
-        public StatLogic(IPhoneLogic phoneLogic, ICustomerLogic customerLogic, IRepairerLogic repairerLogic)
+        ICustomerRepository customerRepo;
+        IPhoneRepository phoneRepo;
+        IRepairerRepository repairerRepo;
+
+        //public StatLogic(IPhoneLogic phoneLogic, ICustomerLogic customerLogic, IRepairerLogic repairerLogic)
+        //{
+        //    this.phoneLogic = phoneLogic;
+        //    this.customerLogic = customerLogic;
+        //    this.repairerLogic = repairerLogic;
+        //}
+
+        public StatLogic(ICustomerRepository customerRepo, IPhoneRepository phoneRepo, IRepairerRepository repairerRepo)
         {
-            this.phoneLogic = phoneLogic;
-            this.customerLogic = customerLogic;
-            this.repairerLogic = repairerLogic;
+            this.customerRepo = customerRepo;
+            this.phoneRepo = phoneRepo;
+            this.repairerRepo = repairerRepo;
         }
 
         //Melyik customer vásárolt összesen a legnagyobb értékben és mennyibe került összesen
         //Which Customer payed the most summerized, and the summerized cost
+        //public KeyValuePair<Customer, double> CustomerWithHighestPriceSummed()
+        //{
+        //    var q = from x in phoneLogic.ReadAll()
+        //            group x by x.CustomerId into g
+        //            select new KeyValuePair<int, double>
+        //            (g.Key ,g.Sum(c => c.Price) ?? 0);
+
+        //    q.OrderBy(x => x.Value);
+
+        //    List<KeyValuePair<Customer, double>> result = new List<KeyValuePair<Customer, double>>();
+        //    foreach (var item in q)
+        //    {
+        //        KeyValuePair<Customer,double> it = new KeyValuePair<Customer,double>(customerLogic.Read(item.Key), item.Value);
+        //        result.Add(it);
+        //    }
+
+        //    return result.FirstOrDefault();
+        //}
+
+
+        //---
         public KeyValuePair<Customer, double> CustomerWithHighestPriceSummed()
         {
-            var q = from x in phoneLogic.ReadAll()
+            var q = from x in phoneRepo.ReadAll()
                     group x by x.CustomerId into g
                     select new KeyValuePair<int, double>
-                    (g.Key ,g.Sum(c => c.Price) ?? 0);
+                    (g.Key, g.Sum(c => c.Price) ?? 0);
 
             q.OrderBy(x => x.Value);
 
             List<KeyValuePair<Customer, double>> result = new List<KeyValuePair<Customer, double>>();
             foreach (var item in q)
             {
-                KeyValuePair<Customer,double> it = new KeyValuePair<Customer,double>(customerLogic.Read(item.Key), item.Value);
+                KeyValuePair<Customer, double> it = new KeyValuePair<Customer, double>(customerRepo.Read(item.Key), item.Value);
                 result.Add(it);
             }
 
