6f1ed310eab0f7c6ca739f8a964ae955d665a303
b.gyorfi <b.gyorfi@twinner.com>
2022-11-23 11:51:33 +0100

    MainWindowViewModel added

---
+++ HX0KAT_HFT_2021222.WPFClient/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HX0KAT_HFT_2021222.WPFClient
+{
+    public class MainWindowViewModel
+    {
+    }
+}
