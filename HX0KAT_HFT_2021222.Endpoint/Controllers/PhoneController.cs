246cd45176f3d6a8f3650eb034b4113124df094f
Győrfi Bence <gyorfi.bence@gmail.com>
2022-11-25 16:42:08 +0100

    Phone Editor added

--- HX0KAT_HFT_2021222.Endpoint/Controllers/PhoneController.cs
+++ HX0KAT_HFT_2021222.Endpoint/Controllers/PhoneController.cs
@@ -1,21 +1,26 @@
-﻿using HX0KAT_HFT_2021222.Logic.Interfaces;
+﻿using Castle.Core.Resource;
+using HX0KAT_HFT_2021222.Endpoint.Services;
+using HX0KAT_HFT_2021222.Logic.Interfaces;
 using HX0KAT_HFT_2021222.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HX0KAT_HFT_2021222.Endpoint.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class PhoneController : ControllerBase
     {
         IPhoneLogic pl;
+        IHubContext<SignalRHub> hub;
 
-        public PhoneController(IPhoneLogic pl)
+        public PhoneController(IPhoneLogic pl, IHubContext<SignalRHub> hub)
         {
             this.pl = pl;
+            this.hub = hub;
         }
 
         // GET: api/<PhoneController>
@@ -37,6 +42,7 @@ namespace HX0KAT_HFT_2021222.Endpoint.Controllers
         public void Post([FromBody] Phone phone)
         {
             pl.Create(phone);
+            this.hub.Clients.All.SendAsync("PhoneCreated", phone);
         }
 
         // PUT api/<PhoneController>
@@ -44,13 +50,16 @@ namespace HX0KAT_HFT_2021222.Endpoint.Controllers
         public void Update([FromBody] Phone phone)
         {
             pl.Update(phone);
+            this.hub.Clients.All.SendAsync("PhoneUpdated", phone);
         }
 
         // DELETE api/<PhoneController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var phoneToDelete = this.pl.Read(id);
             pl.Delete(id);
+            this.hub.Clients.All.SendAsync("PhoneDeleted", phoneToDelete);
         }
     }
 }
---
+++ HX0KAT_HFT_2021222.WPFClient/Views/PhoneEditorView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using HX0KAT_HFT_2021222.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HX0KAT_HFT_2021222.WPFClient.Views
+{
+    /// <summary>
+    /// Interaction logic for PhoneEditorView.xaml
+    /// </summary>
+    public partial class PhoneEditorView : Window
+    {
+        public Phone CurrentPhone { get; set; }
+        public PhoneEditorView(Phone phone)
+        {
+            InitializeComponent();
+            this.DataContext = phone;
+            CurrentPhone = phone;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in stackpanel.Children)
+            {
+                if (item is TextBox t)
+                {
+                    t.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+                }
+            }
+            CurrentPhone = (Phone)this.DataContext;
+            DialogResult = true;
+        }
+    }
+}
